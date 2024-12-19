519ea8766e9e1f0362c60fa19ccf176b551f5a7d
Mark Eaton <MarkEEaton@users.noreply.github.com>
2024-12-19 11:14:28 -0500

    add api_base_url to next Mastodon() call

--- toot.py
+++ toot.py
@@ -2,19 +2,21 @@ import os
 import random
 from mastodon import Mastodon
 
+base_url = 'https://mastodon.ocert.at'
+
 app = Mastodon.create_app(
     'pytooterapp',
-    api_base_url = 'https://mastodon.ocert.at',
+    api_base_url = base_url,
 )
 
 mastodon = Mastodon(client_id = app[0], client_secret = app[1], api_base_url =
-                    'https://mastodon.ocert.at')
+                    base_url)
 token = mastodon.log_in(
     os.environ['WHY_EMAIL'],
     os.environ['WHY_PWD'],
 )
 
-mastodon = Mastodon(access_token = token)
+mastodon = Mastodon(access_token = token, api_base_url = base_url)
 
 
 with open('output.txt', 'r') as infile:
