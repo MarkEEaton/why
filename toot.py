db52ebadc9781c9a6766e7b41fe835d363bd1036
Mark Eaton <MarkEEaton@users.noreply.github.com>
2025-07-19 00:29:07 -0400

    new auth

--- toot.py
+++ toot.py
@@ -2,22 +2,8 @@ import os
 import random
 from mastodon import Mastodon
 
-base_url = 'https://mastodon.ocert.at'
 
-app = Mastodon.create_app(
-    'pytooterapp',
-    api_base_url = base_url,
-)
-
-mastodon = Mastodon(client_id = app[0], client_secret = app[1], api_base_url =
-                    base_url)
-token = mastodon.log_in(
-    os.environ['WHY_EMAIL'],
-    os.environ['WHY_PWD'],
-)
-
-mastodon = Mastodon(access_token = token, api_base_url = base_url)
-
+mastodon = Mastodon(access_token = os.environ['PYTOOTERUSERCREDSECRET'])
 
 with open('output.txt', 'r') as infile:
     toots = infile.readlines()
